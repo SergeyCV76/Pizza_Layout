aaba95427f41a520171d27a0da16f48b5dec8bd6
SergeyCV76 <SergeyCV76@gmail.com>
2024-10-16 19:12:01 +0300

    Add sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require('path');
 const { Template } = require('webpack');
 
 module.exports = {
-  entry: './src/js/main.js',  
+  entry: './src/js/main.js',
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
@@ -15,10 +17,28 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'src', 'main.html'),
-      })
+      template: path.resolve(__dirname, 'src', 'main.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
   ],
-  optimization: {
-    runtimeChunk: 'single',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,        
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          "sass-loader"
+        ],
+      },
+    ],
   },
+
 };
